7f005ac76375f4db7953409807bdc1b465a95151
safayet nur <safayet@merchantbay.com>
2024-01-23 17:58:56 +0600

    first commit

---
+++ src/app/(home)/HeroSlider.jsx
@@ -0,0 +1,40 @@
+// "use client";
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// // Import Swiper styles
+// import mainSlider from "@/data/mainSlider";
+// import { Autoplay, EffectFade, Navigation } from "swiper";
+// import "swiper/css/bundle";
+// import SingleHeroSlider from "./SingleHeroSlider";
+
+// const HeroSlider = () => {
+//   return (
+//     <section className="main-slider">
+//       <Swiper
+//         slidesPerView={1}
+//         loop
+//         navigation
+//         effect="fade"
+//         autoplay
+//         modules={[Navigation, EffectFade, Autoplay]}
+//       >
+//         {mainSlider.map((slider) => (
+//           <SwiperSlide key={slider.id}>
+//             <SingleHeroSlider slider={slider} />
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </section>
+//   );
+// };
+
+// export default HeroSlider;
+
+import React from "react";
+
+const HeroSlider = () => {
+  return <div>HeroSlider</div>;
+};
+
+export default HeroSlider;
